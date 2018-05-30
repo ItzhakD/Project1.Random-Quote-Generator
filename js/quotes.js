3042663ff2887d1f1f453b5939e0768b50ac13da
Itzhak Dangoor <itzhak.dangoor@gmail.com>
2018-05-30 14:28:29 +0300

    Initial commit

---
+++ js/quotes.js
@@ -0,0 +1,160 @@
+const quotes = [
+    {
+        quote: "You can do anything but not everything",
+        wikiLink: "https://en.wikipedia.org/wiki/David_Allen_(author)",
+        source: "David Allen",
+        citation: "Making It All Work",
+        year: 2009,
+        profession: "Management consultant and author"
+    },
+    {
+        quote: "Things don't just happen, they are made to happen",
+        wikiLink: "https://en.wikipedia.org/wiki/John_F._Kennedy",
+        source: "John F. Kennedy",
+        citation: "Speech at Arkansas State Fairground, Little Rock, United States of America",
+        year: 1963,
+        profession: "35th President of the United States"
+    },
+    {
+        quote: "My call is not to those who believe they belong to the past. My call is to those who believe in the future.",
+        wikiLink: "https://en.wikipedia.org/wiki/John_F._Kennedy",
+        source: "John F. Kennedy",
+        citation: "Speech at Civic Auditorium, Seattle, Washington",
+        year: 1960,
+        profession: "35th President of the United States"
+    },
+    {
+        quote: "We can have faith in the future only if we have faith in ourselves",
+        wikiLink: "https://en.wikipedia.org/wiki/John_F._Kennedy",
+        source: "John F. Kennedy",
+        citation: "Presidential Nomination Acceptance Speech to the Democratic National Convention",
+        year: 1960,
+        profession: "35th President of the United States"
+    },
+    {
+        quote: "I, at any rate, am convinced that He does not throw dice.",
+        wikiLink: "https://en.wikipedia.org/wiki/Albert_Einstein",
+        source: "Albert Einstein",
+        citation: "The Born-Einstein Letters",
+        year: 1926,
+        profession: "Theoretical physicist"
+    },
+    {
+        quote: "A man is but the product of his thoughts. What he thinks, he becomes.",
+        wikiLink: "https://en.wikipedia.org/wiki/Mahatma_Gandhi",
+        source: "Mahatma Gandhi",
+        citation: "In Ethical Religion",
+        year: 1922,
+        profession: "Indian activist"
+    },
+    {
+        quote: "The mass of a body is a measure of its energy content.",
+        wikiLink: "https://en.wikipedia.org/wiki/Albert_Einstein",
+        source: "Albert Einstein",
+        citation: "Annalen der Physik",
+        year: 1905,
+        profession: "Theoretical physicist"
+    },
+    {
+        quote: "Life is like riding a bicycle. To keep your balance you must keep moving.",
+        wikiLink: "https://en.wikipedia.org/wiki/Albert_Einstein",
+        source: "Albert Einstein",
+        citation: "Letter to his son Eduard",
+        year: 1930,
+        profession: "Theoretical physicist"
+    },
+    {
+        quote: "When I do good I feel good, when I do bad I feel bad, and that's my religion.",
+        wikiLink: "https://en.wikipedia.org/wiki/Abraham_Lincoln",
+        source: "Abraham Lincoln",
+        citation: "Herndon's Lincoln",
+        year: 1890,
+        profession: "16th President of the United States"
+    },
+    {
+        quote: "As rain breaks through an ill-thatched house, passion will break through an unreflecting mind.",
+        wikiLink: "https://en.wikipedia.org/wiki/Gautama_Buddha",
+        source: "Gautama Buddha",
+        citation: "Dhammapada aritcle",
+        profession: "Founder of Buddhism"
+    },
+    {
+        quote: "Those who dare to fail miserably can achieve greatly.",
+        wikiLink: "https://en.wikipedia.org/wiki/John_F._Kennedy",
+        source: "John F. Kennedy",
+        profession: "35th President of the United States"
+    },
+    {
+        quote: "The true sign of intelligence is not knowledge but imagination.",
+        wikiLink: "https://en.wikipedia.org/wiki/Albert_Einstein",
+        source: "Albert Einstein",
+        profession: "Theoretical physicist"
+    },
+    {
+        quote: "The coldest winter I ever spent was a summer in San Francisco.",
+        wikiLink: "https://en.wikipedia.org/wiki/Mark_Twain",
+        source: "Mark Twain",
+        profession: "American writer"
+    },
+    {
+        quote: "The search for happiness is one of the chief sources of unhappiness.",
+        wikiLink: "https://en.wikipedia.org/wiki/Eric_Hoffer",
+        source: "Eric Hoffer",
+        profession: "American moral and social philosopher"
+    },
+    {
+        quote: "All a musician can do is to get closer to the sources of nature, and so feel that he is in communion with the natural laws.",
+        wikiLink: "https://en.wikipedia.org/wiki/John_Coltrane",
+        source: "John Coltrane",
+        profession: "American jazz saxophonist and composer"
+    },
+    {
+        quote: "Folks are usually about as happy as they make their minds up to be",
+        wikiLink: "https://en.wikipedia.org/wiki/Abraham_Lincoln",
+        source: "Abraham Lincoln",
+        profession: "16th President of the United States"
+    },
+    {
+        quote: "Life is more fun if you play games.",
+        wikiLink: "https://en.wikipedia.org/wiki/Roald_Dahl",
+        source: "Roald Dahl",
+        profession: "British novelist and fighter pilot"
+    },
+    {
+        quote: "Age is a question of mind over matter. If you don't mind, it doesn't matter.",
+        wikiLink: "https://en.wikipedia.org/wiki/Satchel_Paige",
+        source: "Leroy 'Satchel' Paige",
+        profession: "American baseball pitcher"
+    },
+    {
+        quote: "You don't have to be a beer drinker to play darts, but it helps.",
+        source: "Unknown"
+    },
+    {
+        quote: "I still have a full deck; I just shuffle slower now.",
+        source: "Unknown"
+    },
+    {
+        quote: "The best way to succeed in life is to act on the advice we give to others",
+        source: "Unknown"
+    },
+    {
+        quote: "The secret to creativity is knowing how to hide your sources.",
+        source: "Unknown"
+    },
+];
+
+
+
+
+/*
+{
+    quote: "",
+    wikiLink: "",
+    source: "",
+    citation: "",
+    year: 1,
+    profession: ""
+},
+*/
+
---
+++ js/script.js
@@ -0,0 +1,127 @@
+//return value from setInterval function, it is numeric non-zero value
+let intervalId = 0;
+let showAnotherQuoteButtonClicked = false;
+let lastRandomIndex = -1;
+//Though the body background color is "known", it is better to not hard code it, to separate program entities.
+const bodyElem = document.body;
+let lastBackgroundColor = window.getComputedStyle(bodyElem, null).getPropertyValue("background-color");
+
+function getRandomQuote() {
+    let newRandomIndex;
+    //the indexes range is always between 0 to (quotes.length-1), with the help: Math.floor
+    do {
+        newRandomIndex = Math.floor(Math.random() * (quotes.length));
+    } while(lastRandomIndex === newRandomIndex);
+    lastRandomIndex = newRandomIndex;
+    return quotes[newRandomIndex];
+}
+
+function getRandomColor() {
+    //generating color number range except white and black(the lowest & highest rgb)
+    return Math.floor(Math.random() * 254) + 1;
+}
+
+function generateBackgroundColor() {
+    let newBackgroundColor;
+    do {
+        newBackgroundColor = `rgb(${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()})`;
+    } while(newBackgroundColor === lastBackgroundColor);
+    lastBackgroundColor = newBackgroundColor;
+    document.body.style.backgroundColor = newBackgroundColor;
+}
+
+function printQuote() {
+    let html = '';
+    let randomQuote = getRandomQuote();
+
+    //if the user clicked the button then the interval is cleared and the counter is set again
+    if(showAnotherQuoteButtonClicked === true) {
+        clearInterval(intervalId);
+        intervalId = setInterval(printQuote, 8000);
+        showAnotherQuoteButtonClicked = false;
+    }
+
+    //this is the same as the next html quote construction in switch case
+    /*
+    html += `<p class="quote"> ${randomQuote.quote} </p>\n`;
+    if(randomQuote.wikiLink) {
+        html += `<p class="source"><a class="wikiLink" href="${randomQuote.wikiLink}" target="_blank"`;
+        html += ` alt="${randomQuote.source} Wikipedia link" title ="Wikipedia link"> ${randomQuote.source} </a>\n`;
+    } else {
+        html += `<p class="source"> ${randomQuote.source}\n`;
+    }
+
+    //in case of not defined properties the condition evaluate to false
+    if(randomQuote.citation) {
+        html += `<span class="citation"> ${randomQuote.citation} </span>\n`;
+    }
+    if(randomQuote.year) {
+        html += `<span class="year"> ${randomQuote.year} </span>\n`;
+    }
+    if(randomQuote.profession) {
+        html += `<span class="profession"> ${randomQuote.profession} </span>\n`;
+    }
+    html += `</p>`;
+    */
+
+
+    /* The order of the elements print to the screen are kept, due to quotes properties order
+    quote
+    source OR Wikipedia link
+    citation
+    year
+    profession */
+    html += `<p class="quote"> ${randomQuote.quote} \n</p>`;// \n is for the console.log use only!
+    for(let propName in randomQuote) {
+        switch(propName) {
+            case 'source':
+                if(!randomQuote.wikiLink) {
+                    html += `<p class="source"> ${randomQuote.source}\n`;
+                }
+                break;
+            case 'wikiLink':
+                html += `<p class="source"><a class="wikiLink" href="${randomQuote.wikiLink}" target="_blank"`;
+                html += ` alt="${randomQuote.source} Wikipedia link" title ="Wikipedia link"> ${randomQuote.source} </a>\n`;
+                break;
+            case 'citation':
+                html += `<span class="citation"> ${randomQuote.citation} </span>\n`;
+                break;
+            case 'year':
+                html += `<span class="year"> ${randomQuote.year} </span>`;
+                break;
+            case 'profession':
+                html += `<span class="profession"> ${randomQuote.profession} </span>\n`;
+                break;
+        }
+    }
+    html += `</p>`;
+
+    document.getElementById('quote-box').innerHTML = html;
+    generateBackgroundColor();
+}
+
+function showAnotherQuoteClicked() {
+    showAnotherQuoteButtonClicked = true;
+    printQuote();
+}
+
+//  ***** main function that run the application *****
+function main() {
+
+    /* I call printQuote function for the case when opening the page for the first time:
+       - Quote will be random
+       - Page background color will be random */
+    printQuote();
+
+    //for the first time when the program loads the interval is set.
+    //after clicking the button the interval is cleared and set again
+    intervalId = setInterval(printQuote, 8000);
+
+    // event listener to respond to "Show another quote" button clicks.
+    // when user clicks anywhere on the button, the "showAnotherQuoteClicked" is called and
+    // "printQuote" function inside the function above is called
+    document.getElementById('loadQuote').addEventListener("click", showAnotherQuoteClicked, false);
+}
+
+
+main();
